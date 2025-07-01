1dba1c3d145f7dcb97727c826e929a23390b8232
sanjay <sanjusanjay0444@gmail.com>
2025-07-01 21:58:39 +0530

    Initial commit

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ const allowedOrigins = [
   'https://auth-2407.netlify.app',
   'https://movies1-frontend.vercel.app',
   'https://1anchormovies.vercel.app',
+  'https://www.1anchormovies.live',
 ];
 
 const corsOptions = {
